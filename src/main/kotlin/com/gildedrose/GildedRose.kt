be1d4b92e2436c91f2fdfae2725033aa99ea29f1
kallaichan <43334157+kallaichan@users.noreply.github.com>
2021-11-25 22:09:01 +0800

    Refactor: separate logic according to item type

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -7,62 +7,64 @@ private const val SULFURAS = "Sulfuras"
 class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (!isAgedBrie(items[i]) && !isBackstagePass(items[i])) {
-                if (items[i].quality > 0) {
-                    if (!isSulfuras(items[i])) {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!isSulfuras(items[i])) {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!isAgedBrie(items[i])) {
-                    if (!isBackstagePass(items[i])) {
-                        if (items[i].quality > 0) {
-                            if (!isSulfuras(items[i])) {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-            }
+        items.forEach { item ->
+            updateQualityOfIndividualItems(item)
         }
     }
 
-    private fun isSulfuras(item: Item) = item.name.contains(SULFURAS, ignoreCase = true)
+    private fun updateQualityOfIndividualItems(item: Item) {
+        if (item.isSulfuras()) {
+            return
+        }
 
-    private fun isBackstagePass(item: Item) = item.name.contains(BACKSTAGE_PASS, ignoreCase = true)
+        if (item.isAgedBrie()) {
+            item.incrementQualityBy(2)
+            decrementSellIn(item)
+            return
+        }
 
-    private fun isAgedBrie(item: Item) = item.name == AGED_BRIE
+        if (item.isBackstagePass()) {
+            item.incrementQualityBy(1)
+            if (item.sellIn < 11) {
+                item.incrementQualityBy(1)
+            }
+            if (item.sellIn < 6) {
+                item.incrementQualityBy(1)
+            }
+            if (item.sellIn < 0) {
+                item.quality = 0
+            }
+            return
+        }
+
+        decrementQuality(item)
+        decrementSellIn(item)
+
+        if (item.sellIn < 0) {
+            decrementQuality(item)
+        }
+    }
+
+    private fun decrementSellIn(item: Item) {
+        item.sellIn--
+    }
+
+    private fun Item.incrementQualityBy(delta: Int) {
+        quality += delta
+        quality = quality.coerceAtMost(50)
+    }
+
+    private fun decrementQuality(item: Item) {
+        if (item.quality > 0) {
+            item.quality--
+        }
+    }
+
+    private fun Item.isSulfuras() = name.contains(SULFURAS, ignoreCase = true)
+
+    private fun Item.isBackstagePass() = name.contains(BACKSTAGE_PASS, ignoreCase = true)
+
+    private fun Item.isAgedBrie() = name == AGED_BRIE
 
 }
 
